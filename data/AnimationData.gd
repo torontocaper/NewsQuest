3feeef6d35e52a50da2567600934dfa002dc9bee
Adam Burns <torontocaper@gmail.com>
2023-03-01 20:31:23 -0500

    made each member of "anims" dictionary into a nested dictionary

--- data/AnimationData.gd
+++ data/AnimationData.gd
@@ -3,14 +3,16 @@ extends Node
 # this variable is assigned through code, so no need to export
 # i still want to add more data to each entry
 # so something like
-# var anims = {
-#	"idle_right" : {
-#		"start_frame" : 0
-#		"end_frame" : 5
-#		"number_of_frames" : end_frame - start_frame # note i'm not sure if this is possible
-# 	}
-# }
-
+## var anims = {
+#	0: {
+#		"name": "idle_right"
+#		"direction": "right"
+#		"state": "idle"
+#		"start frame": 0
+#		"end frame": 5
+#	},
+#	1: ...
+#
 var anims = {}
 
 # these never change, so no need to export
@@ -19,14 +21,18 @@ var directions : PoolStringArray = ["right", "up", "left", "down"]
 #this variable is public so we can add animation states from the inspector
 export var states : PoolStringArray = ["idle", "walk"]
 
-func set_animations():# fill out the "anims" dict with every combination of state + direction
+func set_animations():# fill out "anims" dict with every combination of state + direction
 	var animations_added = 0
 	var states_added = 0
 	var directions_added = 0
 	while states_added < len(states):
 		directions_added = 0
 		while directions_added < len(directions):
-			anims[animations_added] = states[states_added] + "_" + directions[directions_added]
+			anims[animations_added] = {
+				"state": states[states_added],
+				"direction": directions[directions_added],
+				"name": states[states_added] + "_" + directions[directions_added]
+				}
 			animations_added += 1
 			directions_added += 1
 		states_added += 1
@@ -34,3 +40,4 @@ func set_animations():# fill out the "anims" dict with every combination of stat
 func _ready():
 	set_animations()
 	print(len(anims))
+	print(anims)
